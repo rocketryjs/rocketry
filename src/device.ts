129651fc08c91d58872a916e6ef7b297a64586a1
Evelyn Hathaway <git@evelyn.dev>
2019-09-01 15:48:21 -0700

    New: Add initialization method for mixins used in subclasses of Device

--- src/device.ts
+++ src/device.ts
@@ -121,6 +121,9 @@ export default class Device extends EventEmitter {
 
 		this.send = bindDeep(this, send);
 
+		// Call the initializers defined on the subclass (e.x. from mixins)
+		this.constructor.inits.forEach(init => init.call(this));
+
 		// Open connection with device
 		this.open();
 	}
@@ -228,6 +231,35 @@ export default class Device extends EventEmitter {
 		});
 	}
 
+
+	/*
+		Get the initializers defined on the subclass (e.x. from mixins)
+
+		- Is used to run code from mixins when a new instance of any subclass of `Device` is created
+			- Esentially extending the `constructor()`
+		- Intentionally doesn't impelement a method for inheritance
+			- Would be too complicated of an API to traverse the prototype for what would disincentivize properly using mixins
+			- May change in the future as device support grows
+	*/
+	static get inits() {
+		// Throw if called on raw `Device` class
+		// - Is to prevent mixins for subclasses from accidentally polluting the parent class
+		// - May be dropped if there's a need for inits in plugins
+		if (this === Device) {
+			throw Error("You cannot get the initializers of the Device parent class.");
+		}
+
+		// If the subclass doesn't have its own `_inits`
+		if (!Object.prototype.hasOwnProperty.call(this, "_inits")) {
+			// Make an init `Set` on the subclass
+			Object.defineProperty(this, "_inits", {
+				value: new Set(),
+			});
+		}
+
+		// Return the inits
+		return this._inits;
+	}
 
 	static is(object) {
 		return object instanceof this;
